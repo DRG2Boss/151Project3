bc1e76bdd8e226828dc9e1afb1ee23eb4b97faa2
DRG2Boss <113391397+DRG2Boss@users.noreply.github.com>
2022-10-03 19:20:32 -0400

    main modified

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import arcade
 
 arcade.open_window(1200, 900, "Windows 95 Desktop")
+arcade.set_background_color(arcade.color.CYAN)
 
 arcade.start_render()
 # Monitor Border
-arcade.draw_xywh_rectangle_outline(25, 25, 1150, 850, arcade.color.BEIGE, 30)
+arcade.draw_xywh_rectangle_outline(25, 25, 1150, 850, arcade.color.BEIGE, 50)
 
 # Green Grass
+
 
 arcade.finish_render()
 
